597453fee92c34ccdc7a632b7ae5efca2d46b00a
Nils Van Geele <nils.vgeele@gmail.com>
2014-05-27 22:15:07 +0200

    Updated main function

--- src/llvm_clojure_bindings/main.clj
+++ src/llvm_clojure_bindings/main.clj
@@ -5,34 +5,42 @@
         [llvm-clojure-bindings.llvm]
         [clojure.java.shell :only [sh]]))
 
-(defn link-and-run [obj-file]
-  (let [{:keys [out err exit]} (sh "/opt/local/bin/llvm-ld" "--native" obj-file)]
+(defn compile-ir
+  [ir-file]
+  (let [{:keys [out err exit]}
+        (sh "/usr/local/opt/llvm/bin/llc" "-filetype=obj" "-o" "main.o" "main.ir")]
     (when (not= exit 0)
-      (println "Linker Exit Code: " exit))
+      (throw (Exception. (str "Compiler Exit Code:" exit))))
+    (when (= exit 0)
+      (println "Producing object file..."))))
+
+(defn link-and-run [obj-file]
+  (let [{:keys [out err exit]}
+        (sh "/usr/bin/cc" obj-file)]
+    (when (not= exit 0)
+      (throw (Exception. (str "CC Exit Code: " exit))))
     (when (= exit 0)
       (println "Linking binary...")
       (println "Executing binary a.out:")
-      (println (:out (sh "./a.out")))
-      (println "Finished"))))
+      (println (:out (sh "./a.out"))))))
 
 (defn run-test [ast]
   (let [[b mymod] (create)]
-
     (populate! b mymod ast)
-
     (println (repeat 30 \=))
     (LLVMDumpModule mymod)
     (println (repeat 30 \=))
-
-    (LLVMWriteBitcodeToFile mymod "main.o")
-
-    (link-and-run "main.o")
-
-    (LLVMDisposeModule mymod)))
-
+    (LLVMWriteBitcodeToFile mymod "main.ir")
+    (try
+      (do
+        (compile-ir "main.ir")
+        (link-and-run "main.o"))
+      (catch Exception e
+        (println (str (.getMessage e))))
+      (finally
+        (LLVMDisposeModule mymod)))
+    (println "Finished")))
 
 (defn -main []
   (let [input (slurp "input.program")]
-    (run-test (parse-str input))
-    #_ (run-test example-ast/ast1)
-    ))
+    (run-test (parse-str input))))
